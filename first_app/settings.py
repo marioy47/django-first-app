e45bdd83a8229fab58e3b8b56b1e7d11f6b470ed
Mario Yepes <marioy47@gmail.com>
2020-07-09 11:11:47 -0500

    Fixing security issue and adding pylint with autopep8

--- first_app/settings.py
+++ first_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mfu0$gp1i=8ge_*g#n1jqx-owv15@ph(0kien3c!usd^qjtw@='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
